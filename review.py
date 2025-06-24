eb19635d177ba252daf744e978c71c5c79213369
badr0007 <badruzzamasheikh0@gmail..com>
2025-06-24 20:02:26 +0530

    review.py l14>l14 review added

---
+++ review.py
@@ -0,0 +1,14 @@
+number = int(input(" Please Enter any Positive Integer : "))
+if(number % 5 == 0) and (number % 11 == 0) :
+    print("Given Number {0} is Divisible by 5 and 11 ".format(number))
+elif (number % 5!= 0) and  (number % 11!= 0):
+    print("Given Number {0} is not Divisible by 5 and 11".format(number))
+elif (number % 5 == 0):
+    print("Given Number {0} is Divisible by 5".format(number))
+elif (number % 11 == 0):
+    print("Given Number {0} is Divisible by 11".format(number))
+else:
+    print("Given Number {0}  Divisible by 5 and 11".format(number))
+
+    
+## Correct Code Good Work
